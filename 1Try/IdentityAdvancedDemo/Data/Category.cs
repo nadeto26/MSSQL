e48760dfd34a5df5e43dedcf60af622043460dd0
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-03-03 21:54:45 +0200

    Initial

---
+++ 1Try/IdentityAdvancedDemo/Data/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityAdvancedDemo.Data
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        public ICollection<Furnitures> Furnitures { get; set; } = new List<Furnitures>();
+    }
+}
---
+++ 1Try/IdentityAdvancedDemo/Models/AddToCartViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace IdentityAdvancedDemo.Models
+{
+    public class AddToCartViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public string ImageUrl { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; } = null!;
+    }
+}
