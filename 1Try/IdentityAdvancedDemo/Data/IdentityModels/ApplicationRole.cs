d45d3fa599e38e6b9d01c2ffba0764e5639f0f14
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-03-04 11:25:03 +0200

    Initial

--- 1Try/IdentityAdvancedDemo/Data/IdentityModels/ApplicationRole.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace IdentityAdvancedDemo.Data.IdentityModels
-{
-    public class ApplicationRole : IdentityRole<Guid>
-    {
-    }
-}
--- 1Try/IdentityAdvancedDemo/Models/CategoryViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IdentityAdvancedDemo.Models
-{
-    public class CategoryViewModel
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; } = null!;
-    }
-}
