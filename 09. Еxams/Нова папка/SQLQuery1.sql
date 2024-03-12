e041d214ae7a677b7eb497dd673009541f648fba
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-03-12 16:43:28 +0200

    Initial

---
+++ 09. Еxams/Нова папка/SQLQuery1.sql
@@ -0,0 +1,16 @@
+﻿﻿USE [Accounting]
+GO
+
+--- Table: Countries
+SET IDENTITY_INSERT Countries ON
+INSERT INTO Countries (Id, [Name]) VALUES
+(1, 'Austria'),
+(2, 'France'),
+(3, 'Germany'),
+(4, 'Italy'),
+(5, 'Spain'),
+(6, 'Sweden'),
+(7, 'Greece'),
+(8, 'Romania'),
+(9, 'Slovakia')
+SET IDENTITY_INSERT Countries OFF
