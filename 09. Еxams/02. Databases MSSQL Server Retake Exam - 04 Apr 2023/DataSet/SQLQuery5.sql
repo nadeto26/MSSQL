6ff4a09ddb4b843f71f6fd18407c9da0b9f7dce2
nadeto26 <nadezhdakarapetrova@gmail.com>
2024-03-14 16:04:36 +0200

    Initial

---
+++ 09. Еxams/02. Databases MSSQL Server Retake Exam - 04 Apr 2023/DataSet/SQLQuery5.sql
@@ -0,0 +1,9 @@
+--- Table: Categories
+SET IDENTITY_INSERT Categories ON
+INSERT INTO Categories (Id, [Name]) VALUES
+(1, 'Filters'),
+(2, 'Lights'),
+(3, 'ADR'),
+(4, 'Tyres'),
+(5, 'Others')
+SET IDENTITY_INSERT Categories OFF
